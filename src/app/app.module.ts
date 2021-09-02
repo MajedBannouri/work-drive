efd7f8b1c9abd7499c265d9fc833e5695e4bd6d7
MajedBannouri <majed.bannouri12@gmail.com>
2021-09-02 19:16:26 +0100

    💄 angular font awesome setup ..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { ButtonComponent } from './components/button/button.component';
-import { TasksComponent } from './components/tasks/tasks.component';
-import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { ButtonComponent } from "./components/button/button.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
+import { TaskItemComponent } from "./components/task-item/task-item.component";
+
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
   declarations: [
@@ -13,12 +15,10 @@ import { TaskItemComponent } from './components/task-item/task-item.component';
     HeaderComponent,
     ButtonComponent,
     TasksComponent,
-    TaskItemComponent
+    TaskItemComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FontAwesomeModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
